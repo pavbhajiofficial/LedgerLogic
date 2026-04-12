798b4daf63eeaa4a955be9539b38f5ec88272946
pavbhajiofficial <kurapatibhavyasri@gmail.com>
2026-04-12 19:10:01 +0530

    Update report.h

--- report.h
+++ report.h
@@ -1,37 +1,38 @@
 #ifndef REPORT_H
 #define REPORT_H
 
-#include <vector>
 #include "transaction.h"
+#include <string>
+#include <map>
 
-class ExpenseSummary {
+// Abstract base for polymorphism
+class ReportBase {
+public:
+    virtual ~ReportBase() = default;
+    virtual void generate(const std::vector<Transaction>& txns) const = 0;
+    virtual std::string title() const = 0;
+};
+
+class MonthlyReport : public ReportBase {
+public:
+    void generate(const std::vector<Transaction>& txns) const override;
+    std::string title() const override { return "Monthly Expense Report"; }
+};
+
+class CategoryReport : public ReportBase {
+public:
+    void generate(const std::vector<Transaction>& txns) const override;
+    std::string title() const override { return "Category-wise Expense Report"; }
+};
+
+class ReportSystem {
+public:
+    void printMonthly (const std::vector<Transaction>& txns) const;
+    void printCategory(const std::vector<Transaction>& txns) const;
+
 private:
-    std::vector<Transaction> data;
-
-public:
-    ExpenseSummary() = default;
-
-    ExpenseSummary(ExpenseSummary&& other) noexcept {
-        data = std::move(other.data);
-    }
-
-    void add(const Transaction& t);
-};
-
-class Report {
-public:
-    virtual void generate() = 0;
-    virtual ~Report() {}
-};
-
-class MonthlyReport : public Report {
-public:
-    void generate() override;
-};
-
-class CategoryReport : public Report {
-public:
-    void generate() override;
+    MonthlyReport  monthlyRpt;
+    CategoryReport categoryRpt;
 };
 
 #endif
